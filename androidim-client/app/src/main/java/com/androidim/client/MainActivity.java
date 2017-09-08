d64f00ccebce5a3e2cc8885fa23c5f90da55fabc
ddkong <ddkong@ctrip.com>
2017-09-08 18:56:32 +0800

    android-client

--- androidim-client/app/src/main/java/com/androidim/client/MainActivity.java
+++ androidim-client/app/src/main/java/com/androidim/client/MainActivity.java
@@ -1,10 +1,17 @@
 package com.androidim.client;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.jivesoftware.smack.AbstractConnectionClosedListener;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -14,13 +21,14 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private Socket socket;
     private BufferedReader in = null;
     private PrintWriter out = null;
 
     private Button btSend;
+    private Button btConn;
     private EditText etMsg;
 
     @Override
@@ -29,7 +37,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btSend = (Button) findViewById(R.id.bt_send);
+        btConn = (Button) findViewById(R.id.bt_conn);
         etMsg = (EditText) findViewById(R.id.et_msg);
+
+        btConn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startConnection();
+            }
+        });
 
         btSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,9 +54,6 @@ public class MainActivity extends AppCompatActivity {
                 sendMessage(msg);
             }
         });
-
-        startConnection();
-
     }
 
     private void startConnection() {
@@ -48,14 +61,58 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    socket = new Socket(Config.IP_HOST, Config.IP_PORT);
+                    XMPPTCPConnectionConfiguration.Builder config = XMPPTCPConnectionConfiguration.builder();
+                    config.setHost(Config.IP_HOST);              //设置ejabberd主机IP
+                    config.setPort(Config.IP_PORT);                   //设置端口号：默认5222
+                    config.setUsernameAndPassword("admin", "111111");    //设置用户名与密码
+                    config.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);      //禁用SSL连接
+                    config.setSendPresence(true);
+                    config.setDebuggerEnabled(true);
+                    XMPPTCPConnection  xmppConnection = new XMPPTCPConnection(config.build());
+
+
+                    xmppConnection.addConnectionListener(new AbstractConnectionClosedListener() {
+                        @Override
+                        public void connectionTerminated() {
+
+                        }
+
+                        @Override
+                        public void connected(XMPPConnection connection) {
+                            super.connected(connection);
+                        }
+
+                        @Override
+                        public void authenticated(XMPPConnection connection, boolean resumed) {
+                            super.authenticated(connection, resumed);
+                        }
+
+                        @Override
+                        public void reconnectingIn(int seconds) {
+                            super.reconnectingIn(seconds);
+                        }
+
+                        @Override
+                        public void reconnectionFailed(Exception e) {
+                            super.reconnectionFailed(e);
+                        }
+
+                        @Override
+                        public void reconnectionSuccessful() {
+                            super.reconnectionSuccessful();
+                        }
+                    });
+                    xmppConnection.connect();
+                    boolean isConnection  = xmppConnection.isConnected();
+
+                    /*socket = new Socket(Config.IP_HOST, Config.IP_PORT);
                     in = new BufferedReader(new InputStreamReader(socket
                             .getInputStream()));
                     out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
-                            socket.getOutputStream())), true);
+                            socket.getOutputStream())), true);*/
                     receiveMsg();
                     sendHeartBeat();
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -67,37 +124,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 while (true){
+                    sendMessage("1111");
                     try {
                         Thread.sleep(20*1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    sendMessage("1111");
                 }
             }
         }.start();
     }
 
-    String content;
-    private void receiveMsg(){
-        try {
-            while (true) {
-                if (!socket.isClosed()) {
-                    if (socket.isConnected()) {
-                        if (!socket.isInputShutdown()) {
-                            if ((content = in.readLine()) != null) {
-                                content += "\n";
-                                System.out.println(content);
-                            } else {
 
+    private void receiveMsg(){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        if (!socket.isClosed()) {
+                            if (socket.isConnected()) {
+                                if (!socket.isInputShutdown()) {
+                                    String content=null;
+                                    if ((content = in.readLine()) != null) {
+                                        content += "\n";
+                                        System.out.println(content);
+                                    } else {
+
+                                    }
+                                }
                             }
                         }
                     }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        }.start();
     }
 
     private void sendMessage(String msg){
---
+++ androidim-client/app/src/main/java/com/androidim/client/thread/ConnectionThread.java
@@ -0,0 +1,18 @@
+package com.androidim.client.thread;
+
+/**
+ * Created by ddkong on 2017/9/8.
+ */
+
+public class ConnectionThread extends Thread{
+
+
+
+    @Override
+    public void run() {
+
+    }
+
+
+
+}
